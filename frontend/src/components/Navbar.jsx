1df476403093e85114a9a33c68645e0ce802b356
shrestha <shresthachakrabortykgp@gmail.com>
2025-03-11 20:28:20 +0530

    navigration and logout integrated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -41,12 +41,25 @@ const Navbar = () => {
             <img className="w-8 rounded-full" src={assets.profile_pic} alt="" />
             <img className="w-2.5" src={assets.dropdown_icon} alt="" />
             <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
-              <div className="min-w-48 bg-stone-300 rounded flex flex-col gap-4 p-4">
-                <p className="hover:text-black cursor-pointer ">My profile</p>
-                <p className="hover:text-black cursor-pointer ">
+              <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
+                <p
+                  onClick={() => navigate("my-profile")}
+                  className="hover:text-black cursor-pointer "
+                >
+                  My profile
+                </p>
+                <p
+                  onClick={() => navigate("my-appointment")}
+                  className="hover:text-black cursor-pointer "
+                >
                   My Appointment
                 </p>
-                <p className="hover:text-black cursor-pointer ">Logout</p>
+                <p
+                  onClick={() => setToken(false)}
+                  className="hover:text-black cursor-pointer "
+                >
+                  Logout
+                </p>
               </div>
             </div>
           </div>
